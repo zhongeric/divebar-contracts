b9f5e0c280fd6e2847371b0b7a507597e9ae89cd
Eric Zhong <ezhong1900@gmail.com>
2022-01-04 20:16:36 -0500

    Add game end logic

--- contracts/Deposits.sol
+++ contracts/Deposits.sol
@@ -12,7 +12,7 @@ contract Deposits is ReentrancyGuard {
     using SafeMath for uint256;
 
     struct Player {
-        address player;
+        address addr;
         uint256 bet;
         uint256 winnings;
         uint256 timestamp; // The timestamp when the user waved.
@@ -25,9 +25,11 @@ contract Deposits is ReentrancyGuard {
         uint256 pot;
         uint256 avg;
         // array of players
-        mapping(address => Player) players;
+        mapping(uint256 => Player) players;
+        mapping(address => uint256) existingPlayers;
         uint256 playersSize;
         uint256 createdAt;
+        uint256 endingAt;
     }
 
     address payable public owner;
@@ -41,31 +43,43 @@ contract Deposits is ReentrancyGuard {
         owner = payable(msg.sender);
         // Create a new game
         currentGame.id = 0;
-        currentGame.timeLimit = 60;
+        currentGame.timeLimit = 5;
         currentGame.minDeposit = 0.0001 ether;
         currentGame.pot = 0 ether;
         currentGame.avg = 0 ether;
+        currentGame.playersSize = 0;
         currentGame.createdAt = block.timestamp;
+        currentGame.endingAt = block.timestamp + currentGame.timeLimit;
     }
 
     // Function to receive Ether. msg.data must be empty
     receive() external payable {
-        require(msg.value >= currentGame.minDeposit);
+        require(
+            msg.value >= currentGame.minDeposit,
+            "Deposit must be greater than or equal to the minimum deposit"
+        );
+        // all players are new to the game, can only enter once
+        // continue only if the player is not in the game already
+        require(
+            currentGame.existingPlayers[msg.sender] == 0,
+            "You are already in the game"
+        );
+        // make sure the game is not over
+        require(currentGame.endingAt > block.timestamp, "The game has ended");
+
+        // First real player at 1, so non existent players are 0
+        currentGame.players[currentGame.playersSize + 1] = Player({
+            addr: msg.sender,
+            bet: msg.value,
+            winnings: 0,
+            timestamp: block.timestamp
+        });
+        currentGame.existingPlayers[msg.sender] = currentGame.playersSize + 1;
+        currentGame.playersSize += 1;
         // Add deposit to the pot
         currentGame.pot += msg.value;
         console.log("Msg.value: ", msg.value);
         console.log("Pot: ", currentGame.pot);
-        // Add player to the currentGame
-        // I assume any sender is initialized to 0
-        if (currentGame.players[msg.sender].bet == 0) {
-            currentGame.players[msg.sender] = Player({
-                player: msg.sender,
-                bet: msg.value,
-                winnings: 0,
-                timestamp: block.timestamp
-            });
-        }
-        currentGame.players[msg.sender].bet += msg.value;
         // emit Deposit event
         emit Deposit(msg.sender, msg.value);
     }
@@ -78,9 +92,39 @@ contract Deposits is ReentrancyGuard {
         return address(this).balance;
     }
 
+    function secondsRemaining() public view returns (uint256) {
+        if (currentGame.endingAt <= block.timestamp) {
+            return 0; // already there
+        } else {
+            return currentGame.endingAt - block.timestamp;
+        }
+    }
+
     // Function to return currentGame info
-    function getGamePot() public view returns (uint256) {
-        return currentGame.pot;
+    function getGameInfo()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        return (
+            currentGame.id,
+            currentGame.timeLimit,
+            currentGame.minDeposit,
+            currentGame.pot,
+            currentGame.avg,
+            currentGame.playersSize,
+            currentGame.createdAt,
+            currentGame.endingAt
+        );
     }
 
     function withdraw(uint256 _amount) external {
@@ -88,7 +132,27 @@ contract Deposits is ReentrancyGuard {
         payable(msg.sender).transfer(_amount);
     }
 
-    function sendViaCall(address payable _to) public payable {
+    function handleGameOver() public payable {
+        require(secondsRemaining() == 0, "Game is not over yet");
+        payoutWinnings();
+        // create a new game
+        currentGame.id = currentGame.id + 1;
+        currentGame.createdAt = block.timestamp;
+        currentGame.endingAt = block.timestamp + currentGame.timeLimit;
+    }
+
+    function payoutWinnings() private {
+        // Iterate through players
+        for (uint256 i = 1; i <= currentGame.playersSize; i++) {
+            // send winnings to player
+            sendViaCall(payable(currentGame.players[i].addr));
+            // emit Payout event
+            emit Payout(currentGame.players[i].addr, 0.1 ether);
+            console.log("Payout sent to: ", currentGame.players[i].addr);
+        }
+    }
+
+    function sendViaCall(address payable _to) internal {
         // Call returns a boolean value indicating success or failure.
         // This is the current recommended method to use.
         (bool sent, bytes memory data) = _to.call{value: msg.value}("");
--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,13 @@
 const { ethers } = require("hardhat");
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+}
+
 const main = async () => {
-    const [owner, addr1] = await ethers.getSigners();
+    const [owner, addr1, addr2] = await ethers.getSigners();
     const depositContractFactory = await ethers.getContractFactory('Deposits');
     const depositContract = await depositContractFactory.deploy({
         value: ethers.utils.parseEther('0.5'),
@@ -25,9 +31,9 @@ const main = async () => {
     let addr1Balance = await ethers.provider.getBalance(addr1.address);
     console.log('addr1 balance:', ethers.utils.formatEther(addr1Balance));
 
-    // Add a new deposit
+    // Add a new deposit from addr1
     // Send ether to the contract
-    const depositAmount = ethers.utils.parseEther('0.5');
+    const depositAmount = ethers.utils.parseEther('9000');
     await addr1.sendTransaction({
         to: depositContract.address,
         value: depositAmount,
@@ -36,8 +42,46 @@ const main = async () => {
     /*
         * Get Game info
     */
-    let gamePot = await depositContract.getGamePot();
-    console.log('Game pot:', ethers.utils.formatEther(gamePot));
+    let gameInfo = await depositContract.getGameInfo();
+    console.log('Game info:', gameInfo);
+
+    // Add a new deposit from addr2
+    // Send ether to the contract
+    const depositAmountAddr2 = ethers.utils.parseEther('9000');
+    await addr2.sendTransaction({
+        to: depositContract.address,
+        value: depositAmountAddr2,
+    });
+    console.log(`Sent ${ethers.utils.formatEther(depositAmountAddr2)} to depositContract from addr2`);
+    /*
+        * Get Game info
+    */
+    gameInfo = await depositContract.getGameInfo();
+    console.log('Game info:', gameInfo);
+
+    await sleep(5000);
+
+    // game should be over now, call handleGameOver
+    await depositContract.handleGameOver();
+    console.log('Game over');
+
+    // Verify new game has been created
+    gameInfo = await depositContract.getGameInfo();
+    console.log('Game info:', gameInfo);
+
+    // // Payout winnings
+    // const payoutAmount = ethers.utils.parseEther('420');
+    // await depositContract.payoutWinnings({
+    //     value: payoutAmount,
+    // });
+    // console.log('Paid out winnings');
+
+    // // get balances of addr1 and addr2
+    // addr1Balance = await ethers.provider.getBalance(addr1.address);
+    // console.log('addr1 balance:', ethers.utils.formatEther(addr1Balance));
+
+    // addr2Balance = await ethers.provider.getBalance(addr2.address);
+    // console.log('addr2 balance:', ethers.utils.formatEther(addr2Balance));
   };
   
   const runMain = async () => {
